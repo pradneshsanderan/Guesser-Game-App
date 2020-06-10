5a66993b9ee5f91d691a5ae758f0b54ecc114d36
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-06-10 17:34:35 +0000

    new Commit

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -3,11 +3,12 @@ import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Ale
 import Card from '../components/Card';
 import colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 
 const StartGameScreen = props => {
-    const [enteredValue, setEnteredValue]= useState('');
-    const [confirmed, setConfirmed] =useState(false);
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState();
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
@@ -16,46 +17,53 @@ const StartGameScreen = props => {
         setEnteredValue('');
         setConfirmed(false);
     };
-    const confirmInputHandler =() => {
+    const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if(chosenNumber === NaN || chosenNumber <=0 || chosenNumber>99){
-            Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99',[{text:'Okay', style:'destructive', onPress:resetInputHandler}])
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99', [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }])
             return;
-        } 
+        }
         setConfirmed(true);
         setEnteredValue('');
         setSelectedNumber(chosenNumber);
+        Keyboard.dismiss();
     };
     let confirmedOutput;
-    if(confirmed) {
-        confirmedOutput= 
+    if (confirmed) {
+        confirmedOutput =
+            (
+                <Card style={styles.summary}>
+                    <Text>You Selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="Start Game"/>
+                </Card>);
     }
 
     return (
         <TouchableWithoutFeedback onPress={() => {
             Keyboard.dismiss();
         }}>
-        <View style={styles.screen}>
-            <Text style={styles.title}> Start a New Game! </Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <Input style={styles.input} 
-                blurOnSubmit 
-                autoCapitalize='none' 
-                autoCorrect={false} 
-                keyboardType="number-pad" 
-                maxLength={2}
-                onChangeText={numberInputHandler}
-                value={enteredValue}/>
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                    <Button title="Reset" onPress={resetInputHandler} color={colors.accent}/>
-                    <Button title="Confirm" onPress={confirmInputHandler} color={colors.primary} />
+            <View style={styles.screen}>
+                <Text style={styles.title}> Start a New Game! </Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType="number-pad"
+                        maxLength={2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue} />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={resetInputHandler} color={colors.accent} />
+                            <Button title="Confirm" onPress={confirmInputHandler} color={colors.primary} />
+                        </View>
                     </View>
-                </View>
-                {confirmedOutput}
-            </Card>
-        </View>
+                    {confirmedOutput}
+                </Card>
+            </View>
         </TouchableWithoutFeedback>
     );
 };
@@ -76,7 +84,7 @@ const styles = StyleSheet.create({
         width: '300',
         maxWidth: '80%',
         alignItems: 'center',
-    
+
 
     },
     buttonContainer: {
@@ -85,13 +93,18 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         paddingHorizontal: 15
     },
-    button:{
-        width:100,
-        
+    button: {
+        width: 100,
+
     },
-    input:{
-        width:50,
-        textAlign:'center'
+    input: {
+        width: 50,
+        textAlign: 'center'
+    },
+    summary:{
+        marginTop:20,
+        alignItems:'center',
+
     }
 });
 
